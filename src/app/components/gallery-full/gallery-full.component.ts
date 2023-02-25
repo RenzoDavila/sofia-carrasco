5e245742db8c8294643d87e1ae092adbb7a0d5e7
Renzo Davila <renzo.davila@tecsup.edu.pe>
2023-02-25 11:15:02 -0500

    mejorando responsivo

--- src/app/components/gallery-full/gallery-full.component.ts
+++ src/app/components/gallery-full/gallery-full.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-full',
   templateUrl: './gallery-full.component.html',
   styleUrls: ['./gallery-full.component.sass']
 })
-export class GalleryFullComponent {
+export class GalleryFullComponent implements OnInit {
+  @Input() galleryData:any;
+  showCount = true;
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage:any;
+  currentIndex = 0;
+  controls = true;
+  totalImageCount = 0;
+
+  ngOnInit(): void {
+    this.currentLightboxImage = this.galleryData[0];
+    this.totalImageCount = this.galleryData.length;
+  }
+
+  onPreviewImage(index: any): void{
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightboxImage = this.galleryData[index];
+  }
+
+  onClosePreview(){
+    this.previewImage = false;
+    this.showMask = false;
+  }
+
+  next(){
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex > this.galleryData.length - 1){
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
+
+  prev(){
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0){
+      this.currentIndex = this.galleryData.length - 1;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
 
 }
